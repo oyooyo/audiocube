fe4f93053dc9290bbf71b4fe4f603e663439c339
Joachim Schäfer <joachim.schaefer@gmx.net>
2020-12-29 15:38:22 +0100

    Add create_nfc_file command for LIDL Storyland

--- audiocube.py
+++ audiocube.py
@@ -39,6 +39,35 @@ def convert_file_paths(input_file_paths, output_file_pattern, convert_chunk_func
 		))
 		convert_file_path(input_file_path, output_file_path, convert_chunk_function)
 
+def create_csv_file_content(rows, **csv_writer_kwargs):
+	from csv import writer
+	from io import StringIO
+	with StringIO() as writeable:
+		csv_writer = writer(writeable, **csv_writer_kwargs)
+		csv_writer.writerows(rows)
+		return writeable.getvalue()
+
+def record_to_tagwriter_csv_row(record):
+	return [
+		record.get('type', 'Text'),
+		record['content'],
+		record.get('uri_type', 'en'),
+		record.get('description', record['content']),
+		record.get('interaction_counter', 'no'),
+		record.get('uid_mirror', 'no'),
+		record.get('interaction_counter_mirror', 'no'),
+	]
+
+def create_tagwriter_csv_file_content(records):
+	return create_csv_file_content([
+		['Type (Link, Text)', 'Content (http://....)', 'URI type (URI, URL, File...)', 'Description', 'Interaction counter', 'UID mirror', 'Interaction counter mirror'],
+		*[record_to_tagwriter_csv_row(record) for record in records]
+	], delimiter=';')
+
+def write_text_file(text_file_path, content):
+	with open(text_file_path, 'w') as text_file:
+		text_file.write(content)
+
 def add_input_file_paths_argument(argument_parser):
 	argument_parser.add_argument(
 		'input_file_paths',
@@ -136,6 +165,43 @@ class Audiocube(Simple_Encrypted_File_Device_Type):
 class LIDL_Storyland(Simple_Encrypted_File_Device_Type):
 	def __init__(self):
 		super().__init__('storyland', 'LIDL Storyland', [0x01, 0x80, 0x04, 0x04], 3, '.SMP', '.mp3')
+	def add_commands(self, command_subparsers):
+		super().add_commands(command_subparsers)
+		create_nfc_file_parser = command_subparsers.add_parser(
+			'create_nfc_file',
+			description = 'Create a NFC tag content (.csv) file, to be written via the "NXP TagWriter" app',
+			formatter_class = ArgumentDefaultsHelpFormatter,
+		)
+		create_nfc_file_parser.add_argument(
+			'audio_file_id',
+			type = str,
+			help = 'The audio file ID, a hexadecimal string in range 0000...FFFF. This value determines which audio file will be played if the NFC tag is placed on the device',
+		)
+		create_nfc_file_parser.add_argument(
+			'audio_file_description',
+			type = str,
+			nargs = '?',
+			help = 'The audio file description. Optional, determines which text will be shown in the "NXP TagWriter" app',
+		)
+		create_nfc_file_parser.set_defaults(func=self.create_nfc_file)
+	def create_nfc_file(self, args):
+		audio_file_id = int(args.audio_file_id, 16)
+		description = value_with_default(args.audio_file_description, 'L{:04X}'.format(audio_file_id))
+		csv_file_path = '{description}.csv'.format(
+			description = description,
+		)
+		write_text_file(csv_file_path, create_tagwriter_csv_file_content([{
+			'type': 'Text',
+			'content': '02200408{file_id_high:02X}{file_id_low:02X}00'.format(
+				file_id_high = ((audio_file_id >> 8) & 0xFF),
+				file_id_low = (audio_file_id & 0xFF),
+			),
+			'uri_type': 'en',
+			'description': description,
+			'interaction_counter': 'no',
+			'uid_mirror': 'no',
+			'interaction_counter_mirror': 'no',
+		}]))
 
 class Migros_Storybox(Simple_Encrypted_File_Device_Type):
 	def __init__(self):
